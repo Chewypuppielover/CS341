af8a5e6bfd9d81250f6519f744b2709a8b4e6563
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2020-05-09 13:28:18 -0600

    prove3

---
+++ web/Prove04.php
@@ -0,0 +1,45 @@
+<?php
+   start_session();
+   if(!isset($_Session["cart"])){
+      $_Session["cart"] = array();
+   }      
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset = "utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Store Products </title>
+      <!--<link rel="stylesheet" href=".css">-->
+      <!--<script src="jsFuncts.js"> </script>-->
+      <script type='text/javascript'>
+         function addToCart(item){
+            <?php 
+               $index = count($_Session["cart"]);
+               $_Session["cart"][$index] = item;
+            ?>
+         }
+      </script>
+      <style></style>
+   </head>
+   <body>
+      <div style="display:flex">
+         <table>
+           <th> Products </th>
+           <tr>
+               <td> <label><input type="checkbox" name="products" value="i1" id="i1" onclick="total('i1')">Broken TV </td><td> $5 </td></label>
+               <td> <label><input type="checkbox" name="products" value="i5" id="i5" onclick="total('i5')">Banana PB&J sandwich </td><td> $10 </td></label>
+           </tr>
+           <tr>
+               <td> <label><input type="checkbox" name="products" value="i4" id="i4" onclick="total('i4')">Pirate Magnet </td><td> $5 </td></label>
+               <td> <label><input type="checkbox" name="products" value="i3" id="i3" onclick="total('i3')">Grapes </td><td> $15 </td></label>
+           </tr>
+           <tr>
+               <td> <label><input type="checkbox" name="products" value="i6" id="i6" onclick="total('i6')">
+                       Answer to Life, the universe, and everything </td><td> $42 </td></label>
+               <td> <label><input type="checkbox" name="products" value="i2" id="i2" onclick="total('i2')">Jar Jar </td><td> $20 </td></label>
+           </tr>
+       </table>
+      </div>
+   </body>
+</html>
