75f7cd46bfac0ebb4c873dc907c00ca0838acbb5
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2020-05-09 16:06:56 -0600

    Prove3

--- web/Prove03_cart.php
+++ web/Prove03_cart.php
@@ -2,20 +2,19 @@
    session_start();
    if(!isset($_Session["cart"])){
       $_Session["cart"] = array();
-      $_Session["products"] = array("Broken TV", "JarJar", "Pirate Magnet", "Cleric", "Sorcerer", "Ranger", "Druid");
    }
-   
-   function addToCart(item){
-      $index = count($_Session["cart"]);
-      $_Session["cart"][$index] = item;
-   }  
+
+   function removeFromCart(item){
+      $index = array_search($item, $_Session["cart"]);
+      $_Session["cart"][$index] = null;
+   }
 ?>
 <!DOCTYPE html>
 <html>
    <head>
       <meta charset = "utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Store Products </title>
+      <title>Products Cart </title>
       <!--<link rel="stylesheet" href=".css">-->
       <!--<script src="jsFuncts.js"> </script>-->
       <script type='text/javascript'></script>
@@ -26,15 +25,13 @@
          <h1>Sally's Terrible Store </h1>
          <a href="Prove03_cart.php"> Cart </a>
       </header>
-      <hr/>
-      <div style="display:flex">
          <table>
-            <th> Products </th>
+            <th> Products in Cart</th>
             <?php
                $count = 0;
-               foreach($_Session["products"] as $item) {
+               foreach($_Session["cart"] as $item) {
                   if($count == 0) echo "<tr>";
-                  echo "<td> $item <br> <button onclick=\"addToCart($item)\">Add to Cart</button> </td>";
+                  echo "<td> $item <br> <button onclick=\"removeFromCart($item)\">Remove From Cart</button> </td>";
                   $count += 1;
                   if($count == 3) {
                      $count = 0;
@@ -44,6 +41,5 @@
                if($count != 0) echo "</tr>";
             ?>
          </table>
-      </div>
    </body>
 </html>
