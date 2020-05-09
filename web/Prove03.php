8ed5195a89292caf6a37832d55e7601d8c8d460c
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2020-05-09 15:29:19 -0600

    Prove3

--- web/Prove03.php
+++ web/Prove03.php
@@ -32,7 +32,6 @@
       <div style="display:flex">
          <table>
             <th> Products </th>
-            
             <?php
                $count = 0;
                foreach($_Session["products"] as $item) {
@@ -46,20 +45,6 @@
                }
                if($count != 0) echo "</tr>";
             ?>
-            
-           <tr>
-               <td> <label><input type="checkbox" name="products" value="i1" id="i1" onclick="total('i1')">Broken TV </td><td> $5 </td></label>
-               <td> <label><input type="checkbox" name="products" value="i5" id="i5" onclick="total('i5')">Banana PB&J sandwich </td><td> $10 </td></label>
-           </tr>
-           <tr>
-               <td> <label><input type="checkbox" name="products" value="i4" id="i4" onclick="total('i4')">Pirate Magnet </td><td> $5 </td></label>
-               <td> <label><input type="checkbox" name="products" value="i3" id="i3" onclick="total('i3')">Grapes </td><td> $15 </td></label>
-           </tr>
-           <tr>
-               <td> <label><input type="checkbox" name="products" value="i6" id="i6" onclick="total('i6')">
-                  Answer to Life, the universe, and everything </td><td> $42 </td></label>
-               <td> <label><input type="checkbox" name="products" value="i2" id="i2" onclick="total('i2')">Jar Jar </td><td> $20 </td></label>
-           </tr>
          </table>
       </div>
    </body>
