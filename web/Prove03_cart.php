042b0f3fa3efd140115c0a546454186405e111a2
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2020-05-09 22:42:46 -0600

    Prove3

--- web/Prove03_cart.php
+++ web/Prove03_cart.php
@@ -1,9 +1,6 @@
 <?php
    session_start();
-   if(!isset($_Session["cart"])){
-      $_Session["cart"] = array("No items in Cart");
-   }
-
+   //if(!isset($_Session["cart"])) $_Session["cart"] = array("No items in Cart");
    function removeFromCart($item){
       $index = array_search($item, $_Session["cart"]);
       $_Session["cart"][$index] = null;
@@ -28,17 +25,20 @@
       <table>
          <th> Products in Cart</th>
          <?php
-            $count = 0;
-            foreach($_Session["cart"] as $item) {
-               if($count == 0) echo "<tr>";
-               echo "<td> $item <br> <button onclick=\"removeFromCart($item)\">Remove From Cart</button> </td>";
-               $count += 1;
-               if($count == 3) {
-                  $count = 0;
-                  echo "</tr>";
+            if(!isset($_Session["cart"])) echo "<tr><td> No items in Cart </td></tr>";
+            else {
+               $count = 0;
+               foreach($_Session["cart"] as $item) {
+                  if($count == 0) echo "<tr>";
+                  echo "<td> $item <br> <button onclick=\"removeFromCart($item)\">Remove From Cart</button> </td>";
+                  $count += 1;
+                  if($count == 3) {
+                     $count = 0;
+                     echo "</tr>";
+                  }
                }
+               if($count != 0) echo "</tr>";
             }
-            if($count != 0) echo "</tr>";
          ?>
       </table>
       <?php print_r($_Session["cart"]); ?>
