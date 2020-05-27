19a9fe57ab5dc34266a6e1d2966998928945ff6d
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2020-05-27 15:31:35 -0600

    misc corrections

--- web/index.php
+++ web/index.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset = "utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title> Suzy Naegle's Assignment Index </title>
-	<style>
-	   div {
-	      margin: auto;
-	      max-width: 100%;
-	      width: fit-content;
-	   }
-	</style>
-    </head>
-    <body>
+   <head>
+      <meta charset = "utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title> Suzy Naegle's Assignment Index </title>
+      <style>
+         div {
+            margin: auto;
+            max-width: 100%;
+            width: fit-content;
+         }
+      </style>
+   </head>
+   <body>
       <div>
-        <h1>CS 345 Assignments</h1>
-        <ul style="list-style-position:inside;">
-            <li> <a href="Prove02.html"> Home Page </a> </li>
-            <li> <a href="Prove03.php"> Shopping Cart </a></li>
+         <h1>CS 345 Assignments</h1>
+         <ul style="list-style-position:inside;">
+            <li> <a href="Prove01.html"> Hello World     </a> </li>
+            <li> <a href="Prove02.html"> Home Page       </a> </li>
+            <li> <a href="Prove03.php" > Shopping Cart   </a></li>
             <li> <a href="Prove04.html">  </a></li>
             <li> <a href="Prove05.html">  </a></li>
             <li> <a href="Prove06.html">  </a></li>
@@ -26,9 +27,9 @@
             <li> <a href="Prove10.html">  </a></li>
             <li> <a href="Prove11.html">  </a></li>
             <li> <a href="Prove12.html">  </a></li>
-        </ul>
+         </ul>
       </div>
-    </body>
+   </body>
 </html>
 
 <?php
