7e59adc17ea123eb74982114a8b9313283c36432
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2020-05-27 16:09:22 -0600

    teaches added

--- web/Team03.php
+++ web/Team03.php
@@ -1,21 +1,22 @@
 <?php
-    $name = $_POST["Name"];
-    $email = $_POST["Email"];
-    $major = $_POST["Major"];
-    $comments = $_POST["Comments"];
-    $countries = $_POST["Loc"];
+   $name = $_POST["Name"];
+   $email = $_POST["Email"];
+   $major = $_POST["Major"];
+   $comments = $_POST["Comments"];
+   $countries = $_POST["Loc"];
 ?>
 <html>
-  <head>
-    <title> Team02 php </title>
-  </head>
-  <body>
-    <?php echo "Name: $name </br>"; 
-          echo "Email: $email <a href=\"$email\">Send email</a> </br>";
-          echo "Major: $major </br>";
-          echo "$comments </br>";
-          echo "Has been to: ";//$countries";
-          foreach($countries as $country){echo "$country, ";}
-    ?>
-  </body>
+   <head>
+      <title> Team02 php </title>
+   </head>
+   <body>
+      <?php 
+         echo "Name: $name </br>"; 
+         echo "Email: $email <a href=\"$email\">Send email</a> </br>";
+         echo "Major: $major </br>";
+         echo "$comments </br>";
+         echo "Has been to: ";//$countries";
+         foreach($countries as $country){echo "$country, ";}
+      ?>
+   </body>
 </html>
