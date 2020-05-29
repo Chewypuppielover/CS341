b309d76736c1ea58e64a53e397ca7d65b971e620
Suzy Naegle <chewypuppielover@yahoo.com>
2020-05-28 20:19:19 -0600

    Prove3 clear cart

--- web/Prove03_cart.php
+++ web/Prove03_cart.php
@@ -74,9 +74,9 @@
          window.onload = function () {
             var DEBUG = false;
             if(DEBUG) alert("JS is Working, page loaded");
-            //document.getElementById("clr").addEventListener("click", function(){reset('button');} );
+            document.getElementById("clr").addEventListener("click", function(){reset('button');} );
             document.getElementById("info").addEventListener("change", function(){reset('div');} );
-         });
+         };
          
          function reset(source) {
             alert("reset called by " + source);
